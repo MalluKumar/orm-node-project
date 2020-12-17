c822b0b2097b17ca6f0dbbf1a4beaeb8e3de1f40
MalluMalligereKumar <mallumkumar069@gmail.com>
2020-12-17 20:31:41 +1100

    Add validators

--- db/models/movie.js
+++ db/models/movie.js
@@ -7,9 +7,20 @@ module.exports = (sequelize) => {
     // intializing the model and define columns for the table.
     Movie.init({
         id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-        title: { type: Sequelize.STRING, allowNull: false }, // to not insert 'null' keyword for empty fields.
-        runtime: { type: Sequelize.INTEGER, allowNull: false },
-        releaseDate: { type: Sequelize.DATEONLY, allowNull: false },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false, // to not insert 'null' keyword for empty fields.
+            validate: {
+                notEmpty: {
+                    msg: "Please enter value for title."
+                },
+                notNull: {
+                    msg: "Please enter value for title."
+                }
+            } // to validate incorrect records.
+        },
+        runtime: { type: Sequelize.INTEGER, allowNull: false, validate: { notNull: { msg: "Please enter value." }, min: { args: 1, msg: "Provide a value greater than zero." } } },
+        releaseDate: { type: Sequelize.DATEONLY, allowNull: false, validate: { notNull: { msg: "Enter a value." }, isAfter: { args: '1895-12-27', msg: "Provide a value after 1895-12-27" } } },
         isAvailableOnVHS: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false }
     }, { sequelize }); // same as {sequelize : sequelize}
 
