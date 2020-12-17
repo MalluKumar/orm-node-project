00bc1d64ce7dc2f8db896ef769e4b4e64e58ce20
MalluMalligereKumar <mallumkumar069@gmail.com>
2020-12-17 23:30:46 +1100

    Add other way to create and save data in database

--- app.js
+++ app.js
@@ -12,19 +12,23 @@ const { Movie, Person } = db.models; // same as const Movie = db.models.Movie
         await db.sequelize.sync({ force: true }); //force:true drops table if exists and create new table every time.
 
         // Instance of Movie class
-        const movie1 = await Movie.create({
+        const movie1 = await Movie.create({ // creating new record using 'create' method.
             title: "KGF",
             runtime: 80,
             releaseDate: "2020-09-12",
             isAvailableOnVHS: true
         });
 
-        const movie2 = await Movie.create({
+        // 
+        const movie2 = await Movie.build({ // creating new record using 'build' method (Note: call save() method).
             title: "KF",
             runtime: 50,
             releaseDate: "2020-09-23",
             isAvailableOnVHS: true
         });
+
+        // call save method when build() is used, to save data to database. (or else it will be stored only in memory).
+        await movie2.save();
 
         // Instance of Person class
         const person1 = await Person.create({
@@ -35,7 +39,7 @@ const { Movie, Person } = db.models; // same as const Movie = db.models.Movie
         console.log(person1.toJSON());
         console.log(movie1.toJSON());
         console.log(movie2.toJSON());
-
+        
     } catch (error) {
         if (error.name === "SequelizeValidationError") {
             const errors = error.errors.map(err => err.message);
