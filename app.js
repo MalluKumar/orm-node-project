fa87b6847bae9517648f56a32641c07e2cf0e0d5
MalluMalligereKumar <mallumkumar069@gmail.com>
2020-12-17 13:38:50 +1100

    Connect to DB

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+
+// Connecting ORM to database and creating database named movies.db
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "movies.db"
+});
+
+// async IIFE (immediately invoked function expression)
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection to the database is successful!!");
+    } catch (err) {
+        console.error("Error connecting to the database", err);
+    }
+})();
